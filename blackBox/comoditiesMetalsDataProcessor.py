07d1b4f767274874b868c93a21887ed5fddcbea9
Angel Salazar <aangelsalazarr@gmail.com>
2022-09-02 14:57:42 -0500

    ready to print commodities futures pdf

--- blackBox/comoditiesMetalsDataProcessor.py
+++ blackBox/comoditiesMetalsDataProcessor.py
@@ -1,4 +1,10 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import seaborn as sns
+
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
 
 """
 purpose is to intake a list of commodities and run a process of grabbing
@@ -33,3 +39,43 @@ for commodity in commoditiesHistories:
     commodity.drop(['Dividends', 'Stock Splits'], inplace=True, axis=1)
 
 # now we will create multiple figures and put them into one pdf
+fig1 = plt.figure()
+sns.lineplot(data=goldHistory, x='Date', y='Open', color='green')
+ax2_1 = plt.twinx()
+sns.lineplot(data=goldHistory, x='Date', y='Volume', color='lightgreen')
+
+fig2 = plt.figure()
+sns.lineplot(data=platinumHistory, x='Date', y='Open', color='green')
+ax2_2 = plt.twinx()
+sns.lineplot(data=platinumHistory, x='Date', y='Volume', color='lightgreen')
+
+fig3 = plt.figure()
+sns.lineplot(data=palladiumHistory, x='Date', y='Open', color='green')
+ax2_3 = plt.twinx()
+sns.lineplot(data=palladiumHistory, x='Date', y='Volume', color='lightgreen')
+
+fig4 = plt.figure()
+sns.lineplot(data=silverHistory, x='Date', y='Open', color='green')
+ax2_4 = plt.twinx()
+sns.lineplot(data=silverHistory, x='Date', y='Volume', color='lightgreen')
+
+fig5 = plt.figure()
+sns.lineplot(data=copperHistory, x='Date', y='Open', color='green')
+ax2_5 = plt.twinx()
+sns.lineplot(data=copperHistory, x='Date', y='Volume', color='lightgreen')
+
+fig6 = plt.figure()
+sns.lineplot(data=aluminumHistory, x='Date', y='Open', color='green')
+ax2_6 = plt.twinx()
+sns.lineplot(data=aluminumHistory, x='Date', y='Volume', color='lightgreen')
+
+def save_multi_image(filename):
+    pp = PdfPages(filename)
+    fig_nums = plt.get_fignums()
+    figs = [plt.figure(n) for n in fig_nums]
+    for fig in figs:
+        fig.savefig(pp, format='pdf')
+    pp.close()
+
+filename = 'CommoditiesFuturesInformation.pdf'
+save_multi_image(filename)
