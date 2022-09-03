2bddc0d7d9bc419f08d5f12a75018bdab04b557a
Angel Salazar <aangelsalazarr@gmail.com>
2022-09-02 20:20:40 -0500

    commodities metals data processor essentially finalized

--- blackBox/comoditiesMetalsDataProcessor.py
+++ blackBox/comoditiesMetalsDataProcessor.py
@@ -2,6 +2,8 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import seaborn as sns
+from matplotlib import rc
+rc('mathtext', default='regular')
 
 # plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
@@ -40,34 +42,50 @@ for commodity in commoditiesHistories:
 
 # now we will create multiple figures and put them into one pdf
 fig1 = plt.figure()
-sns.lineplot(data=goldHistory, x='Date', y='Open', color='green')
+golda = sns.lineplot(data=goldHistory, x='Date', y='Open',
+                     color='green').set(title=gold.info['shortName'])
 ax2_1 = plt.twinx()
-sns.lineplot(data=goldHistory, x='Date', y='Volume', color='lightgreen')
+goldb = sns.lineplot(data=goldHistory, x='Date', y='Volume', color='lightgreen')
 
 fig2 = plt.figure()
-sns.lineplot(data=platinumHistory, x='Date', y='Open', color='green')
+platinuma = sns.lineplot(data=platinumHistory, x='Date', y='Open',
+                    color='green').set(
+    title=platinum.info['shortName'])
 ax2_2 = plt.twinx()
-sns.lineplot(data=platinumHistory, x='Date', y='Volume', color='lightgreen')
+platinumb = sns.lineplot(data=platinumHistory, x='Date', y='Volume',
+                  color='lightgreen')
 
 fig3 = plt.figure()
-sns.lineplot(data=palladiumHistory, x='Date', y='Open', color='green')
+palladiuma = sns.lineplot(data=palladiumHistory, x='Date', y='Open',
+                   color='green').set(
+    title=palladium.info['shortName'])
 ax2_3 = plt.twinx()
-sns.lineplot(data=palladiumHistory, x='Date', y='Volume', color='lightgreen')
+palladiumb = sns.lineplot(data=palladiumHistory, x='Date', y='Volume',
+                 color='lightgreen')
 
 fig4 = plt.figure()
-sns.lineplot(data=silverHistory, x='Date', y='Open', color='green')
+silvera = sns.lineplot(data=silverHistory, x='Date', y='Open',
+                      color='green').set(
+    title=silver.info['shortName'])
 ax2_4 = plt.twinx()
-sns.lineplot(data=silverHistory, x='Date', y='Volume', color='lightgreen')
+silverb = sns.lineplot(data=silverHistory, x='Date', y='Volume',
+                    color='lightgreen')
 
 fig5 = plt.figure()
-sns.lineplot(data=copperHistory, x='Date', y='Open', color='green')
+coppera = sns.lineplot(data=copperHistory, x='Date', y='Open',
+                      color='green').set(
+    title=lmeCu.info['shortName'])
 ax2_5 = plt.twinx()
-sns.lineplot(data=copperHistory, x='Date', y='Volume', color='lightgreen')
+copperb = sns.lineplot(data=copperHistory, x='Date', y='Volume',
+                    color='lightgreen')
 
 fig6 = plt.figure()
-sns.lineplot(data=aluminumHistory, x='Date', y='Open', color='green')
+aluminuma = sns.lineplot(data=aluminumHistory, x='Date', y='Open',
+                    color='green').set(
+    title=aluminum.info['shortName'])
 ax2_6 = plt.twinx()
-sns.lineplot(data=aluminumHistory, x='Date', y='Volume', color='lightgreen')
+aluminumb = sns.lineplot(data=aluminumHistory, x='Date', y='Volume',
+                  color='lightgreen')
 
 
 def save_multi_image(filename):
