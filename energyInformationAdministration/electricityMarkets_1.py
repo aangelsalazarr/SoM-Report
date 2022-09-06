21f00e4b144cff354328dcdc138ab3ae2608101f
Angel Salazar <aangelsalazarr@gmail.com>
2022-09-05 22:25:32 -0500

    almost there with grabbing relevant energy data

--- energyInformationAdministration/electricityMarkets_1.py
+++ energyInformationAdministration/electricityMarkets_1.py
@@ -12,15 +12,6 @@ USA
 # first we want to retrieve our api key and make sure it is good to go
 apiKey = os.environ.get('eiaAPI')
 
-# api endpoint; will be adding specifics at the end of URL
-URL = "https://api.eia.gov/v2/electricity/"
-
-# here are the specifics of what we are looking for
-specificReq = "electricity/retail-sales/data/"
-
-# electricity pricing API endpoint
-electricityPrices = URL + specificReq
-
 '''
 here we are setting up our time range, overall we should not worry about the end
 year because if left as null it is assumed it will grab the most recent
@@ -29,14 +20,39 @@ going back 5 years max
 '''
 currentDate = date.today()
 startDate = currentDate - relativedelta(years=5)
-startDateEIA = startDate.strftime("%Y-%m")
+startDateEIA = startDate.strftime("%Y-%m-%d")
 
+# api endpoint; will be adding specifics at the end of URL
+URL = "https://api.eia.gov/v2/"
+
+# here are the specifics of what we are looking for
+specificReq = "electricity/retail-sales/data"
+
+# electricity pricing API endpoint
+electricityPricesURL = URL + specificReq + "?api_key=" + str(apiKey)
+
+"""
+it is proving difficult at the moment to use HTTPS header so we will look at our
+PARAMS and slowly convert it and add it to our url since our case is specific
+in this python script
+"""
+
+dataInput = "&data[0]=customers&data[1]=price&data[2]=revenue&data[3]=sales"
+facetsInput= "&facets[sectorid][0]=ALL&facets[sectorid][1]=COM&facets[" \
+             "sectorid]=IND&facets[sectorid]=OTH&facets[sectorid]=RES&facets[" \
+             "sectorid]=TRA"
+frequencyInput = "&frequency=monthly"
+endDateInput = "&start=" + str(startDateEIA)
+
+# adding everything up
+electricityPricesURL = electricityPricesURL + dataInput + facetsInput \
+                       +frequencyInput + endDateInput
 
 # purpose is to define parameters
 PARAMS = {
     'frequency': 'monthly',
     'data': [
-        'customers', 'price', 'revenue', 'sales'
+        "customers", "price", "revenue", "sales"
     ],
     'facets': {
         'sectorid': [
@@ -55,3 +71,13 @@ PARAMS = {
     'length': 5000,
     'api-version': '2.0.2'
 }
+
+# sending get request and saving the response as a response object
+r = requests.get(url=electricityPricesURL)
+
+# extracting data in json format
+data = r.json()
+
+# visualizing data
+print(electricityPricesURL)
+print(data)
