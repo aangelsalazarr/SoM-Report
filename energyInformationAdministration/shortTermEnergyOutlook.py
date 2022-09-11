237d8d335664dac92aa026406831cb572f254c2f
Angel Salazar <aangelsalazarr@gmail.com>
2022-09-10 22:11:55 -0500

    settingup seo

---
+++ energyInformationAdministration/shortTermEnergyOutlook.py
@@ -0,0 +1,110 @@
+import os
+
+# loading our API key
+apiKey = os.environ.get('eiaAPI')
+
+'''
+Graph Titles To Create
+Figure 1:
+    - West Texas Intermediate (WTI) crude oil price and NYMEX confidence 
+    intervals
+Figure 2:
+    - Graph 1: U.S. gasoline and crude oil prices
+    - Graph 2: components of annual gasoline price changes
+Figure 3: 
+    - Graph 1: U.S. diesel and crude oil prices
+    - Graph 2: components of annual diesel prices changes
+Figure 4: 
+    - Henry hub natural gas price and NYMEX confidence intervals
+Figure 5: 
+    - U.S. natural gas prices
+Figure 6: 
+    - world liquid fuels production and consumption balance
+Figure 7:
+    - Estimated unplanned liquid fuels production outages among OPEC and non-OPEC producers
+Figure 8:
+    -
+Figure 9:
+    -
+Figure 10:
+    -  
+Figure 11:
+    -
+Figure 12:
+    -
+Figure 13:
+    -
+Figure 14:
+    -
+Figure 15:
+    -
+Figure 16:
+    -
+Figure 17:
+    -
+Figure 18:
+    -
+Figure 19:
+    -
+Figure 20:
+    -
+Figure 21:
+    -
+Figure 22:
+    -
+Figure 23:
+    -
+Figure 24:
+    -
+Figure 25:
+    -
+Figure 26:
+    -
+Figure 27:
+    -
+Figure 28:
+    -
+Figure 29:
+    -
+Figure 30: 
+    -
+Figure 31:
+    -
+Figure 32:
+    -
+Figure 33:
+    -
+Figure 34:
+    -
+Figure 35:
+    -
+Figure 36:
+    -
+Figure 37:
+    -
+Figure 38:
+    -
+Figure 39:
+    -
+Figure 40: 
+    -
+Figure 41:
+    -
+Figure 42:
+    -
+Figure 43:
+    -
+Figure 44:
+    -
+Figure 45:
+    -
+Figure 46:
+    -
+Figure 47:
+    -
+Figure 48:
+    -
+Figure 49:
+    -
+     
+'''
