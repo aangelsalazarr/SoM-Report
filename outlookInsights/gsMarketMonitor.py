89e0b913a42b9753b8fd8dc1980049e2a622ec0d
Angel Salazar <aangelsalazarr@gmail.com>
2022-09-19 10:12:45 -0500

    setting up process to grab gs market monitor pdf

---
+++ outlookInsights/gsMarketMonitor.py
@@ -0,0 +1,8 @@
+
+'''
+URL: https://www.gsam.com/content/dam/gsam/pdfs/common/en/public/articles/global-market-monitor/2022/market_monitor_091622.pdf?sa=n
+
+urlSegment1 = https://www.gsam.com/content/dam/gsam/pdfs/common/en/public/articles/global-market-monitor/
+urlSegment2 = 2022/market_monitor_091622.pdf?sa=n
+
+'''
