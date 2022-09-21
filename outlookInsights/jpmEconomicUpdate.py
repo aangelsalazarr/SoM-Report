044deac895379dbde246a659a8504f456b1bb630
Angel Salazar <aangelsalazarr@gmail.com>
2022-09-21 14:52:53 -0500

    including aspect of logging jpmeconmonitor script

--- outlookInsights/jpmEconomicUpdate.py
+++ outlookInsights/jpmEconomicUpdate.py
@@ -16,15 +16,19 @@ eoUrl = 'https://am.jpmorgan.com/content/dam/jpm-am-aem/americas/us/en' \
 
 response = requests.get(eoUrl)
 
+file = open(r'.\outlookLogger\jpmEconMonitorLogger.txt', 'a')
+
+
 if response.status_code == 200:
     file_path = os.path.join(output_dir, str(today_reformat) + '_' +
     os.path.basename(eoUrl))
-    # file.write(f'{datetime.datetime.now()}: A pdf successfully downloaded.
-    # \n')
+    file.write(f'{datetime.datetime.now()}: A pdf successfully downloaded. \n')
+
     with open(file_path, 'wb') as f:
         f.write(response.content)
+
 else:
-    # file.write(f'{datetime.datetime.now()}: the script ran without pdf '
-               # f'download. \n')
+    file.write(f'{datetime.datetime.now()}: the script ran without pdf '
+               f'download. \n')
     print(f"Response: {response}")
     print("No PDF found.")
