21512fa26c9a399478abc4aaffdb0246792a031a
Angel Salazar <aangelsalazarr@gmail.com>
2022-09-28 14:51:34 -0500

    now working on a webscraper to grab data from today in energy from the EIA website

---
+++ market/inflationVisuals.py
@@ -0,0 +1,18 @@
+'''
+Purpose of the script is to present some key visuals as it relates to inflation
+1. Inflation over time, USD
+    a. data: [1] CPI, CPI (seasonally adjusted), PPI, GDP Deflator
+    b. data: [2] SD in CPI
+    c. x axis: YTD, 3 years, 5 years, and 10 years
+2. Inflation Expectations
+    a. data: [1] UMichigan Survey, consumer expecations of inflation
+    b. data: [2] NY fed survey of consumer expectation
+3. TreasuryMarket Expected Inflation
+    a. data: [1] implied expected inflation, T. Bond Rate, TIPs rate
+    b. dataSetup: [1] delta(tbond, tips) == measure of market expexted inflation
+    c. x axis: 1 year
+4. Expected inflation term sttructure
+    a. extension of [3]
+    b. topics(lines on graph): current month, prevous month, YTD
+    c. YTD data: 3 curves for the year, aka delta(TBond, TIPs) from 1Y to 30Y
+'''
