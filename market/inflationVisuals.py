8d8f1c66cd7dadb4c932a4469b3253fce9c4313b
Angel Salazar <aangelsalazarr@gmail.com>
2022-09-28 16:00:40 -0500

    inflation visualizer : getting gdp deflator data from FRED

--- market/inflationVisuals.py
+++ market/inflationVisuals.py
@@ -16,3 +16,25 @@ Purpose of the script is to present some key visuals as it relates to inflation
     b. topics(lines on graph): current month, prevous month, YTD
     c. YTD data: 3 curves for the year, aka delta(TBond, TIPs) from 1Y to 30Y
 '''
+import os
+import fredapi as fa
+import pandas as pd
+
+# setting up any api keys that we may need
+fredKey = os.environ.get('FREDKEY')
+
+# figure 1
+cpi = "CUUR0000SA0"  # series id from bls
+cpi_adj = "CUSR0000SA0" # searies id from bls, seasonally adjusted
+ppi = 0
+
+# grabbing gdp deflator data
+fred = fa.Fred(api_key=fredKey)
+gdp_deflator = fred.get_series("GDPDEF")  # percent change from a year ago
+gdp_deflator.name = 'GDP Def'
+
+# converting data from fred into df
+fredDf = pd.DataFrame(data=gdp_deflator, )
+
+print(fredDf)
+
